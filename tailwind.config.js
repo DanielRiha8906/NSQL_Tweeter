d8732ea2fa5d52802e6b10d8288ead32ce0171cc
Martin Kopecký <kopecky@thekrew.app>
2023-11-25 21:02:44 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/css/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
