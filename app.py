37f37762badadd97fbbd5caca8bfa37f678a8674
Daniel Říha <daniel.riha8906@seznam.cz>
2023-11-25 20:20:26 +0100

    Start

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Alexander',
+        'title': 'Pp poo poo',
+        'content': 'It realy do be',
+        'date_posted': '25. November 2023'
+    },
+    {
+        
+        'author': 'Daniel',
+        'title': 'yes',
+        'content': 'pes',
+        'date_posted': '26. November 2023'
+    },
+    {
+        
+        'author': 'Adam',
+        'title': 'nope',
+        'content': 'coke',
+        'date_posted': '27. November 2023'
+    }
+]
+
+@app.route("/")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+if __name__ == '__main__':
+    app.run(debug=True)
