5390137c15ed6cff05231af156654ba1dc6301d5
Daniel Říha <daniel.riha8906@seznam.cz>
2023-11-25 21:08:34 +0100

    Fixed Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./templates/**/*.html",
-    "./static/css/**/*.js"
+    "./templates/*",
+    "./static/css/*"
   ],
   theme: {
     extend: {},
