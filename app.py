307883ca2a78e72f5a1e3f5364d5c9f70bbb89a7
DanielRiha8906 <daniel.riha8906@seznam.cz>
2023-11-27 11:08:15 +0100

    Graphic changes

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ posts = [
         
         'author': 'Daniel',
         'title': 'yes',
-        'content': 'pes',
+        'content': 'Thoughout the history there has been some bullshit, that I am just writing to get a sense of how does it work rn regarding the structure of the text.com',
         'date_posted': '26. November 2023'
     },
     {
