a231db2bc7af8c0a2fcf59afec9b9a0382983542
Bumross <bumr007@gmail.com>
2023-11-28 04:10:43 +0100

    docker zatim se nepripoji, 4:10 AM LMAO

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 posts = [
@@ -9,14 +10,12 @@ posts = [
         'date_posted': '25. November 2023'
     },
     {
-        
         'author': 'Daniel',
         'title': 'yes',
         'content': 'Thoughout the history there has been some bullshit, that I am just writing to get a sense of how does it work rn regarding the structure of the text.com',
         'date_posted': '26. November 2023'
     },
     {
-        
         'author': 'Adam',
         'title': 'nope',
         'content': 'coke',
@@ -31,11 +30,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
 @app.route("/profile")
 def profile():
     return render_template('profile.html')
+
 @app.route("/register")
 def register():
     return render_template('register.html')
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host= "0.0.0.0", port=5000, debug=True)
