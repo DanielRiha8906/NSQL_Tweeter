7bd3d9956a7c3352fae5c34cd31d4dbd239bf36e
Bumross <bumr007@gmail.com>
2023-11-29 21:03:48 +0100

    odstranil jsem explicitne dany posty

--- app.py
+++ app.py
@@ -7,26 +7,6 @@ app = Flask(__name__)
 
 db = F()
 
-posts = [
-    {
-        'author': 'Alexander',
-        'title': 'brb',
-        'content': 'Jdu kourit',
-        'date_posted': '25. November 2023'
-    },
-    {
-        'author': 'Daniel',
-        'title': 'yes',
-        'content': 'Thoughout the history there has been some bullshit, that I am just writing to get a sense of how does it work rn regarding the structure of the text.com',
-        'date_posted': '26. November 2023'
-    },
-    {
-        'author': 'Adam',
-        'title': 'nope',
-        'content': 'coke',
-        'date_posted': '27. November 2023'
-    }
-]
 
 
 @app.route("/")
